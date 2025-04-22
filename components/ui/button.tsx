36dc8157103dfd4704f5cc60913ac62a8894b0c9
Pidelotunfc <pidelotunfc@gmail.com>
2025-04-22 12:11:31 +0200

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,10 +1,1 @@
-export function Button({ children, onClick, className = "" }) {
-  return (
-    <button
-      onClick={onClick}
-      className={`px-3 py-1 rounded-xl border text-sm font-medium ${className}`}
-    >
-      {children}
-    </button>
-  );
-}
+
